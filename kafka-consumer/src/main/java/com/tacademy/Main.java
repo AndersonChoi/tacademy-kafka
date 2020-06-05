539cdd249861b2228573bbdbe3d47f0798dff0f3
AndersonChoi <choco4235@gmail.com>
2020-06-05 20:14:05 +0900

    init commit

---
+++ kafka-consumer/src/main/java/com/tacademy/Main.java
@@ -0,0 +1,17 @@
+package com.tacademy;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello kafka");
+    }
+}
